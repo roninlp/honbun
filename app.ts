6346fadafa5d6dfee5d44c9bd52050eb0a615b54
roninlp <ronin.lp@gmail.com>
2024-05-02 22:05:58 +0330

    add expenses route

--- app.ts
+++ app.ts
@@ -1,5 +1,6 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
+import { expensesRoute } from "./routes/expenses";
 
 const app = new Hono();
 
@@ -8,4 +9,7 @@ app.use("*", logger());
 app.get("/test", (c) => {
   return c.json({ message: "test" });
 });
+
+app.route("/api/expenses", expensesRoute);
+
 export default app;
