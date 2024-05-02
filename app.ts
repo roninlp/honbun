3ed923c51618a637e3442a398fa14704ca459d6a
roninlp <ronin.lp@gmail.com>
2024-05-02 22:01:59 +0330

    add logger

--- app.ts
+++ app.ts
@@ -1,6 +1,10 @@
 import { Hono } from "hono";
+import { logger } from "hono/logger";
 
 const app = new Hono();
+
+app.use("*", logger());
+
 app.get("/test", (c) => {
   return c.json({ message: "test" });
 });
