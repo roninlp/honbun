1ecbf1a9709ad409123450a5bab64e0aa85dbd87
ronin <ronin.lp@gmail.com>
2024-05-04 21:14:06 +0330

    feat: add table for expenses

--- frontend/src/routes/expenses.tsx
+++ frontend/src/routes/expenses.tsx
@@ -1,3 +1,13 @@
+import { Skeleton } from "@/components/ui/skeleton";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 import { api } from "@/lib/api";
 import { queryOptions, useSuspenseQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
@@ -20,19 +30,47 @@ async function getAllExpenses() {
 }
 
 function Expenses() {
-  const { data } = useSuspenseQuery(expensesQueryOptions);
+  const { data, isPending } = useSuspenseQuery(expensesQueryOptions);
   const expenses = data.expenses;
 
   return (
-    <div>
+    <div className="mx-auto max-w-3xl p-2">
       <h1>Expenses</h1>
-      <ul>
-        {expenses.map((expense) => (
-          <li key={expense.id}>
-            {expense.title}: ${expense.amount}
-          </li>
-        ))}
-      </ul>
+      <Table>
+        <TableCaption>A list of all your expenses.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">Id</TableHead>
+            <TableHead>Title</TableHead>
+            <TableHead>Amo unt</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {isPending
+            ? Array(3)
+                .fill(0)
+                .map((_, i) => (
+                  <TableRow key={i}>
+                    <TableCell className="font-medium">
+                      <Skeleton className="h-4" />
+                    </TableCell>
+                    <TableCell>
+                      <Skeleton className="h-4" />
+                    </TableCell>
+                    <TableCell>
+                      <Skeleton className="h-4" />
+                    </TableCell>
+                  </TableRow>
+                ))
+            : expenses.map((expense) => (
+                <TableRow key={expense.id}>
+                  <TableCell className="font-medium">{expense.id}</TableCell>
+                  <TableCell>{expense.title}</TableCell>
+                  <TableCell>${expense.amount}</TableCell>
+                </TableRow>
+              ))}
+        </TableBody>
+      </Table>
     </div>
   );
 }
